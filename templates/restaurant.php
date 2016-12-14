f67b7793010db2b2d6fea7ab218b489b2244243d
MaggGomes <magg.slb@gmail.cm>
2016-12-14 20:31:04 +0000

    createreviewchanges

--- templates/restaurant.php
+++ templates/restaurant.php
@@ -1,7 +1,7 @@
 <section class="central">
     <div class="container">
         <section class="contentRestaurant">
-            <div class="restaurant box">
+            <section class="restaurant box">
                 <div class="restaurantgallery">
                     <ul class="bxslider">
                         <li><img src="../uploads/restaurants/restaurant1.png"/></li>
@@ -50,7 +50,45 @@
                     </p>
                     <div id="restaurantmap" class="gmap3"></div>
                 </div>
-            </div>
+            </section>
+
+            <section id="addreviewphoto" class="box">
+                <div id="restaurantbtns">
+                    <div class="userimage">
+                        <img src="../uploads/users/<?= $user['photopath'] ?>">
+                    </div>
+                    <?php if (!isset($_SESSION['username']) || $user['status'] == 'reviewer') { ?>
+                        <button type="button" id="btn-createreview" class="btn" title="Criar avaliação">Criar avaliação
+                        </button>
+                    <?php } ?>
+                    <button type="button" id="btn-addphoto" class="btn" title="Adicionar foto">Adicionar foto</button>
+                </div>
+                <form action="../actions/add_review.php" method="post" id="addreview">
+                    <input type="hidden" name="idUsername" value="<?= $_SESSION['username'] ?>">
+                    <input type="hidden" name="idRestaurant" value="<?= $restaurant['id'] ?>">
+                    <section class="createreview">
+                        <br>
+                            <span class="rating">
+                                <input type="radio" name="rating" class="rating-input" id="rating-input-1-5" value="5"/>
+                                <label for="rating-input-1-5" class="rating-star"></label>
+                                <input type="radio" name="rating" class="rating-input" id="rating-input-1-4" value="4"/>
+                                <label for="rating-input-1-4" class="rating-star"></label>
+                                <input type="radio" name="rating" class="rating-input" id="rating-input-1-3" value="3"/>
+                                <label for="rating-input-1-3" class="rating-star"></label>
+                                <input type="radio" name="rating" class="rating-input" id="rating-input-1-2" value="2"/>
+                                <label for="rating-input-1-2" class="rating-star"></label>
+                                <input type="radio" name="rating" class="rating-input" id="rating-input-1-1" value="1"/>
+                                <label for="rating-input-1-1" class="rating-star"></label>
+                            </span>
+                        <br><br>
+                        <label>Opinião:<br>
+                            <textarea rows="4" cols="100" name="text" value=""></textarea>
+                        </label><br>
+                        <input type="submit" id="#btn-createreview" id="btn-submit" class="btn" name="btnSubmit" value="Publicar">
+                    </section>
+                </form>
+            </section>
+
             <section class="restaurantreview">
                 <div class="sectiondescription">
                     <h3>Avaliações
@@ -75,36 +113,5 @@
                 <?php } ?>
             </section>
         </section>
-
-        <?php if (!isset($_SESSION['username']) || $user['status'] == 'reviewer') { ?>
-        <section class="addreviewphoto">
-            <button type="button" id="btn-createreview" class="btn" title="Criar avaliação">Criar avaliação</button>
-            <form action="../actions/add_review.php" method="post" id="addreview">
-                <input type="hidden" name="idUsername" value="<?= $_SESSION['username'] ?>">
-                <input type="hidden" name="idRestaurant" value="<?= $restaurant['id'] ?>">
-                <section class="createreview">
-                    <br>
-                <span class="rating">
-                    <input type="radio" name="rating" class="rating-input" id="rating-input-1-5" value="5"/>
-                    <label for="rating-input-1-5" class="rating-star"></label>
-                    <input type="radio" name="rating" class="rating-input" id="rating-input-1-4" value="4"/>
-                    <label for="rating-input-1-4" class="rating-star"></label>
-                    <input type="radio" name="rating" class="rating-input" id="rating-input-1-3" value="3"/>
-                    <label for="rating-input-1-3" class="rating-star"></label>
-                    <input type="radio" name="rating" class="rating-input" id="rating-input-1-2" value="2"/>
-                    <label for="rating-input-1-2" class="rating-star"></label>
-                    <input type="radio" name="rating" class="rating-input" id="rating-input-1-1" value="1"/>
-                    <label for="rating-input-1-1" class="rating-star"></label>
-                </span>
-                    <br><br>
-                    <label>Opinião:<br>
-                        <textarea rows="4" cols="100" name="text" value=""></textarea>
-                    </label><br>
-                    <input type="submit" id="btn-submit" class="btn" name="btnSubmit" value="Publicar">
-                </section>
-            </form>
-        </section>
-</section>
-<?php } ?>
-</div>
+    </div>
 </section>
