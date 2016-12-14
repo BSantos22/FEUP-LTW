9d763c9213872035c5b204ecb8e1975ec2b8c3b0
José Oliveira <jose.j.p.oliveira.96@gmail.com>
2016-12-14 02:39:40 +0000

    register almost ready

--- scripts/script.js
+++ scripts/script.js
@@ -116,44 +116,83 @@ $(document).ready(function() {
     });
 
     //validation of the register fields
-    $("#reg-user").keyup(function() {
-        if ($("#reg-user").val() == null || $("#reg-user").val() == "")
-            $("#reg-user").css("border", "1px solid #ccc");
-        else if (/^([a-z0-9]*)$/.test($("#reg-user").val()) && /^\S/.test($("#reg-user").val()))
-            $("#reg-user").css("border", "2px solid #3fa246");
-        else
-            $("#reg-user").css("border", "2px solid #c21212");
 
+    var user = false;
+    var name = false;
+    var email = false;
+    var password = false;
+    var birthdate = false;
+    var city = false;
+    var country = false;
+    var type = false;
+
+    function updateSbtBtn() {
+      if(user && name && email && password && birthdate && city && country && type){
+        $("#reg-btn").removeAttr("disabled");
+          $("#reg-btn").css("background-color", "#c21212");
+      }else {
+        $("#reg-btn").css("background-color","#8e8e8e");
+        $("#reg-btn").attr("disabled", true);
+      }
+
+    };
+
+    $("#reg-user").keyup(function() {
+        if ($("#reg-user").val() == null || $("#reg-user").val() == ""){
+            $("#reg-user").css("border", "1px solid #ccc");
+            user = false;
+        }else if (/^([a-z0-9]*)$/.test($("#reg-user").val()) && /^\S/.test($("#reg-user").val())){
+            $("#reg-user").css("border", "2px solid #3fa246");
+            user = true;
+        }else{
+            $("#reg-user").css("border", "2px solid #c21212");
+            user = false;
+        }
+        updateSbtBtn();
     });
 
     $("#reg-name").keyup(function() {
-        if ($("#reg-name").val() == null || $("#reg-name").val() == "")
+        if ($("#reg-name").val() == null || $("#reg-name").val() == ""){
             $("#reg-name").css("border", "1px solid #ccc");
-        else if (/^([^0-9]*)$/.test($("#reg-name").val()))
+            name = false;
+        }else if (/^([^0-9]*)$/.test($("#reg-name").val())){
             $("#reg-name").css("border", "2px solid #3fa246");
-        else
+            name = true;
+        }else{
             $("#reg-name").css("border", "2px solid #c21212");
+            name = false;
+        }
+        updateSbtBtn();
     });
 
     $("#reg-mail").blur(function() {
-        if ($("#reg-mail").val() == null || $("#reg-mail").val() == "")
+        if ($("#reg-mail").val() == null || $("#reg-mail").val() == ""){
             $("#reg-mail").css("border", "1px solid #ccc");
-        else if (	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($("#reg-mail").val()))
+            email = false;
+        }else if (	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($("#reg-mail").val())){
             $("#reg-mail").css("border", "2px solid #3fa246");
-        else
+            email = true;
+        }else{
             $("#reg-mail").css("border", "2px solid #c21212");
+            email = false;
+        }
+        updateSbtBtn();
     });
 
     $("#reg-pass2").blur(function() {
-        if ($("#reg-pass2").val() == null || $("#reg-pass2").val() == "")
+        if ($("#reg-pass2").val() == null || $("#reg-pass2").val() == ""){
             $("#reg-pass2").css("border", "1px solid #ccc");
-        else if($("#reg-pass2").val() == $("#reg-pass1").val()){
+            password = false;
+        }else if($("#reg-pass2").val() == $("#reg-pass1").val()){
             $("#reg-pass1").css("border", "2px solid #3fa246");
             $("#reg-pass2").css("border", "2px solid #3fa246");
+            password = true;
         }else{
             $("#reg-pass1").css("border", "2px solid #c21212");
             $("#reg-pass2").css("border", "2px solid #c21212");
+            password = false;
         }
+        updateSbtBtn();
     });
 
     $("#reg-bdate").focus(function() {
@@ -163,16 +202,61 @@ $(document).ready(function() {
     $("#reg-bdate").blur(function() {
         if ($("#reg-bdate").val() == "") {
             document.getElementById("reg-bdate").type = "text";
+            birthdate = false;
+        }else{
+          $("#reg-bdate").css("border", "2px solid #3fa246");
+          birthdate = true;
         }
+        updateSbtBtn();
     });
 
     $("#reg-bdate").keyup(function() {
-        if ($("#reg-bdate").val() == null || $("#reg-bdate").val() == "")
+        if ($("#reg-bdate").val() == null || $("#reg-bdate").val() == ""){
             $("#reg-bdate").css("border", "1px solid #ccc");
-        else if($("#reg-pass2").val() == $("#reg-pass1").val())
-            $("#reg-pass2").css("border", "2px solid #3fa246");
-        else
-            $("#reg-pass2").css("border", "2px solid #c21212");
+            birthdate = false;
+        }else{
+          $("#reg-bdate").css("border", "2px solid #3fa246");
+          birthdate = true;
+        }
+        updateSbtBtn();
+    });
+
+    $("#city").keyup(function() {
+        if ($("#city").val() == null || $("#city").val() == ""){
+            $("#city").css("border", "1px solid #ccc");
+            city = false;
+        }else if (/^([^0-9]*)$/.test($("#city").val())){
+            $("#city").css("border", "2px solid #3fa246");
+            city = true;
+        }else{
+            $("#city").css("border", "2px solid #c21212");
+            city = false;
+        }
+        updateSbtBtn();
+    });
+
+    $(".country").click(function() {
+        if ($(".country").val() == null || $(".country").val() == ""){
+            $(".country").css("color", "#adadad");
+            country = false;
+        }else{
+          $(".country").css("color", "#000");
+          $(".country").css("border", "2px solid #3fa246");
+          country = true;
+        }
+        updateSbtBtn();
+    });
+
+    $("#sel-ut").click(function() {
+        if ($("#sel-ut").val() == null || $("#sel-ut").val() == ""){
+            $("#sel-ut").css("color", "#adadad");
+            type = false;
+        }else{
+          $("#sel-ut").css("color", "#000");
+          $("#sel-ut").css("border", "2px solid #3fa246");
+          type = true;
+        }
+        updateSbtBtn();
     });
 
 
