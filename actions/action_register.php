356fc6b33c373f9fc13a2158319a26dde4a57fa2
Bruno Santos <bruno.santos1996@gmail.com>
2016-12-15 19:31:40 +0000

    fixed owner register

--- actions/action_register.php
+++ actions/action_register.php
@@ -4,15 +4,8 @@
     include_once('../database/connection.php');
     include_once('../database/user.php');
 
-    if ($_POST['submit'] == 'owner') {
-        $status = 'owner';
-    }
-    else {
-        $status = 'reviewer';
-    }
-
     try {
-        createUser($db, $_POST['username'], $_POST['name'], $_POST['email'], $_POST['password'], $_POST['birthday'], $_POST['city'], $_POST['country'], $status);
+        createUser($db, $_POST['username'], $_POST['name'], $_POST['email'], $_POST['password'], $_POST['birthday'], $_POST['city'], $_POST['country'], $_POST['user_type']);
     } catch(PDOException $e) {
         die($e->getMessage());
     }
