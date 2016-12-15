43ce42b805b976f7dc8676e223594ae3f6795c63
José Oliveira <jose.j.p.oliveira.96@gmail.com>
2016-12-15 02:16:02 +0000

    minor changes valid_email

--- scripts/valid_email.php
+++ scripts/valid_email.php
@@ -4,6 +4,6 @@
     include_once('../database/connection.php');
     include_once('../database/user.php');
 
-    $email = getUserByEmail($db, $_GET['tempEmail']);
-    echo json_encode($user == false);
+    $tempEmail = getUserByEmail($db, $_GET['email']);
+    echo json_encode($tempEmail == false);
 ?>
